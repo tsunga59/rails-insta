8840efb1ef68c733ca2d68549e4c9e0a9983f7e7
tsunga59 <tatsumochi15@gmail.com>
2021-07-18 16:49:24 +0900

    いいね機能用のモデルを作成

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,8 @@
 class User < ApplicationRecord
 
   has_many :posts, dependent: :destroy
+
+  has_many :likes
   
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
